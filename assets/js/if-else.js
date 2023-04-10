f72f9ba46a288eeb9ce054f05a8a5d7e53b14fa1
Gustavo Villegas <villegasgustavoopel@hotmail.com>
2023-04-10 12:26:10 -0300

    logica booleana

--- assets/js/if-else.js
+++ assets/js/if-else.js
@@ -9,7 +9,30 @@ if (a >= 10){
 console.log('Fin del programa')
 
 const hoy = new Date();
-let dia = hoy.getDate();
-
+let dia = hoy.getDay();
+dia = 3
 console.log ({dia})
 
+
+if(dia === 1){
+    console.log('Es Lunes');
+}else if(dia === 0){
+    console.log('Es Domingo')
+}else if (dia === 2){
+    console.log('Martes')
+}else{
+    console.log('No es lunes o martes o Domingo')
+}
+
+const diasLetras = {
+    0:'Domingoo',
+    1:'Luness',
+    2:'Martess',
+    3:'Miercoless',
+    4:'Juevess',
+    5:'vierness',
+    6:'Sabadoo'
+}
+
+const diasLetras2 = ['Domingo','Lunes','Martes','Miercoles','Jueves','viernes','Sabado'];
+console.log(diasLetras[dia])
---
+++ assets/js/logica-booleana.js
@@ -0,0 +1,16 @@
+const regresaTrue = ()=>{
+    console.log('Regresa True');
+    return true;
+}
+
+const regresafalse = ()=>{
+    console.log('Regresa False');
+    return false;
+}
+
+console.warn('Not o la negacion');
+
+console.log(true);//true
+console.log(!false);//true
+console.log(false);//false
+console.log(!true);//false
