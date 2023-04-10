3ecc0e183b1e88ba0751f07e4505a032a21c70d5
Gustavo Villegas <villegasgustavoopel@hotmail.com>
2023-04-10 07:32:49 -0300

    ejercicios-lunes

---
+++ assets/js/if-else.js
@@ -0,0 +1,15 @@
+let a = 5;
+
+if (a >= 10){
+    console.log('A es igual o mayor a 10')
+}else{
+    console.log('A es menor a 10');
+}
+
+console.log('Fin del programa')
+
+const hoy = new Date();
+let dia = hoy.getDate();
+
+console.log ({dia})
+
